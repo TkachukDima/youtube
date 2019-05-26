9bf83b0fe38ff59af9eb53b8f73128803c6a2aec
Dima <tkachuk.dima97@gmail.com>
2019-05-26 21:11:13 +0300

    Updates

---
+++ precache-manifest.04a7f56dcf2d9272ab05156dba10ad0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8db0effa94c990964e",
+    "url": "/youtube/static/js/runtime~main.a6ce4185.js"
+  },
+  {
+    "revision": "87d453e8adc0431d0b00",
+    "url": "/youtube/static/js/main.50b1bca1.chunk.js"
+  },
+  {
+    "revision": "c3e90bc17066e568231f",
+    "url": "/youtube/static/js/2.4067b302.chunk.js"
+  },
+  {
+    "revision": "87d453e8adc0431d0b00",
+    "url": "/youtube/static/css/main.b2a7e5bd.chunk.css"
+  },
+  {
+    "revision": "c5df02e2e2e0ea46b5c5fdb117cbd4af",
+    "url": "/youtube/index.html"
+  }
+];
